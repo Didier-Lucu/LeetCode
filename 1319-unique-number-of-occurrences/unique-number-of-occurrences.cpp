5fdc487965ab5b0facd097e8dffdc88b0fabfea9
Didier-Lucu <118077001+Didier-Lucu@users.noreply.github.com>
2025-10-04 15:52:27 -0700

    Time: 0 ms (100.00%) | Memory: 12 MB (57.27%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -7,6 +7,10 @@ using namespace std;
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+        // First we insert each int into a frequency map to log how many of 
+        // each appear. Then we go through that frequency map inserting each
+        // frequency into a set. Before we insert we check if that frequency 
+        // is already in there. If it is then its false, otherwise its true.
         map<int, int> freq;
         set<int> s;
         int count = 0;
